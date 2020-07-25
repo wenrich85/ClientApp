2c62d17d9696978bd4cf72bfc482c730ba5e4aed
Wendell Richards <wenrich85@gmail.com>
2020-07-24 22:01:53 -0400

    updated case sensitivity

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,41 +1,41 @@
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule } from "@angular/router";
 import { ToastrModule } from "ngx-toastr";
 
-import { SidebarModule } from './sidebar/sidebar.module';
-import { FooterModule } from './shared/footer/footer.module';
-import { NavbarModule } from './shared/navbar/navbar.module';
-import { FixedPluginModule } from './shared/fixedplugin/fixedplugin.module';
+import { SidebarModule } from "./sidebar/sidebar.module";
+import { FooterModule } from "./shared/footer/footer.module";
+import { NavbarModule } from "./shared/navbar/navbar.module";
+import { FixedPluginModule } from "./shared/fixedplugin/fixedplugin.module";
 
-import { AppComponent } from './app.component';
-import { AppRoutes } from './app.routing';
+import { AppComponent } from "./app.component";
+import { AppRoutes } from "./app.routing";
 
-import { LayoutComponent } from './layout/layout.component';
+import { LayoutComponent } from "./layout/layout.component";
 
-import { HttpClientModule } from '@angular/common/http';
-import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from "@angular/common/http";
+import { ReactiveFormsModule } from "@angular/forms";
 
 //Authorization
-import { RecaptchaModule, RecaptchaFormsModule } from 'ng-recaptcha';
-import { Authorization } from '../app/pages/authorization/authorization.component';
-import { VoterVerification } from '../app/pages/authorization/voterVerification/voterVerification.component';
-import { MobileVerification } from '../app/pages/authorization/mobileVerification/mobileVerification.component';
-import { RegistrationComponent } from '../app/pages/authorization/registration/registration.component';
-import { QuestionVerification } from '../app/pages/authorization/questionVerification/questionVerification.component';
+import { RecaptchaModule, RecaptchaFormsModule } from "ng-recaptcha";
+import { Authorization } from "../app/pages/authorization/authorization.component";
+import { VoterVerification } from "../app/pages/authorization/voterVerification/voterVerification.component";
+import { MobileVerification } from "../app/pages/authorization/mobileVerification/mobileVerification.component";
+import { RegistrationComponent } from "../app/pages/authorization/registration/registration.component";
+import { QuestionVerification } from "../app/pages/authorization/questionVerification/questionVerification.component";
 
 //Facial Recognition
-import { FacialRecognition } from '../app/pages/authorization/facialRecognition/facialRecognition.component';
-import { FacialRecognitionService } from '../app/services/facialRecognition.service';
-import { BrowserModule } from '@angular/platform-browser';
-import { BoolToYesNoPipe } from './pipes/bool-to-yes-no.pipe';
+import { FacialRecognition } from "../app/pages/authorization/facialRecognition/facialRecognition.component";
+import { FacialRecognitionService } from "../app/services/facialRecognition.service";
+import { BrowserModule } from "@angular/platform-browser";
+import { BoolToYesNoPipe } from "./pipes/bool-to-yes-no.pipe";
 
 //Ballot Handling
-import { Ballot } from './shared/ballot/ballot.component';
-import { BallotBuilder } from './pages/ballotBuilder/ballotBuilder.component';
-import { BallotCompiler } from './pages/ballotBuilder/ballotcompiler.component';
-import { VoteComponent } from './pages/vote/vote.component';
-import { BallotApprover } from './pages/ballotBuilder/ballotapprover.component';
+import { Ballot } from "./shared/ballot/ballot.component";
+import { BallotBuilder } from "./pages/ballotBuilder/ballotbuilder.component";
+import { BallotCompiler } from "./pages/ballotBuilder/ballotcompiler.component";
+import { VoteComponent } from "./pages/vote/vote.component";
+import { BallotApprover } from "./pages/ballotBuilder/ballotapprover.component";
 
 @NgModule({
   declarations: [
@@ -70,9 +70,7 @@ import { BallotApprover } from './pages/ballotBuilder/ballotapprover.component';
     RecaptchaModule,
     HttpClientModule
   ],
-  providers: [
-    FacialRecognitionService
-  ],
+  providers: [FacialRecognitionService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
