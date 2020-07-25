482c8939d0d93848d739d379d59b4dca5cf449a4
Wendell Richards <wenrich85@gmail.com>
2020-07-24 22:11:26 -0400

    case update

--- src/app/pages/ballotBuilder/ballotbuilder.component.ts
+++ src/app/pages/ballotBuilder/ballotbuilder.component.ts
@@ -1,102 +1,92 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, FormArray, Validators } from '@angular/forms';
-import { BallotService } from '../../services/ballotService';
-
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, FormArray, Validators } from "@angular/forms";
+import { BallotService } from "../../services/ballotService";
 
 @Component({
-    selector: 'ballot-builder',
-    templateUrl: 'ballotBuilder.component.html'
+  selector: "ballot-builder",
+  templateUrl: "ballotbuilder.component.html"
 })
-
 export class BallotBuilder implements OnInit {
-    ballotForm: FormGroup;
+  ballotForm: FormGroup;
 
-    ballot;
+  ballot;
 
-    isDataAvailable = false;
+  isDataAvailable = false;
 
-    constructor(private formBuilder: FormBuilder, private ballotS: BallotService) { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private ballotS: BallotService
+  ) {}
 
-    async ngOnInit() {
-        await this.ballotS.getBallot(7112020, 981, 2)
-        await this.ballotS.ballots.subscribe(ballot => {
-            this.ballot = ballot;
-            console.log(this.ballot)
-        })
-        this.ballotForm = this.formBuilder.group({
-            seatName:  ['', Validators.required],
-            candidates: new FormArray([
-            ])
-        });
+  async ngOnInit() {
+    await this.ballotS.getBallot(7112020, 981, 2);
+    await this.ballotS.ballots.subscribe(ballot => {
+      this.ballot = ballot;
+      console.log(this.ballot);
+    });
+    this.ballotForm = this.formBuilder.group({
+      seatName: ["", Validators.required],
+      candidates: new FormArray([])
+    });
 
-        this.listSeats
-        
-        
+    this.listSeats;
 
-        this.onAddCandidate();
-        this.isDataAvailable = true;
+    this.onAddCandidate();
+    this.isDataAvailable = true;
+  }
+
+  get candidates() {
+    return this.ballotForm.controls.candidates as FormArray;
+  }
+
+  onAddCandidate() {
+    this.candidates.push(
+      this.formBuilder.group({
+        candidateId: [Math.floor(100 + Math.random() * 200)],
+        cFirstName: ["", Validators.required],
+        cLastName: ["", Validators.required],
+        party: ["", [Validators.required]]
+      })
+    );
+  }
+
+  onReset() {
+    this.ballotForm.reset();
+    this.candidates.clear();
+    this.onAddCandidate();
+  }
+
+  seatDict = {};
+
+  listSeats() {
+    for (let i = 0; i < this.ballot.seats.length; i++) {
+      if (!this.seatDict[this.ballot.seats[i].seatName]) {
+        this.seatDict[this.ballot.seats[i].seatName] = i;
+      }
     }
+  }
 
-    get candidates() { return this.ballotForm.controls.candidates as FormArray; }
+  onSubmit() {
+    this.listSeats();
 
-    onAddCandidate(){
-        this.candidates.push(this.formBuilder.group({
-            candidateId:[Math.floor(100 + Math.random() * 200)],
-            cFirstName: ['', Validators.required],
-            cLastName: ['', Validators.required],
-            party: ['', [Validators.required]]
-        }));
+    let payLoad = JSON.parse(JSON.stringify(this.ballotForm.getRawValue()));
+    payLoad["SeatID"] = Math.floor(100 + Math.random() * 900);
+    console.log(payLoad);
+    let index = this.seatDict[payLoad.seatName];
+    if (index !== undefined) {
+      payLoad.candidates.forEach(candidate => {
+        this.ballot.seats[index].candidates.push(candidate);
+        console.log(this.ballot.seats[index].candidates);
+      });
+
+      console.log(payLoad.seatName.toString());
+      this.onReset();
+      return;
     }
+    this.ballot.seats.push(payLoad);
 
-    onReset(){
-        this.ballotForm.reset();
-        this.candidates.clear();
-        this.onAddCandidate();
-    }
+    this.onReset();
 
-    seatDict = {};
-
-    listSeats(){
-        for (let i = 0; i < this.ballot.seats.length; i++){
-         
-            if(!this.seatDict[this.ballot.seats[i].seatName]){
-                this.seatDict[this.ballot.seats[i].seatName] = i 
-            }
-             
-        }
-    }
-
-    onSubmit(){
-       
-        this.listSeats()
-
-        
-        let payLoad = JSON.parse(JSON.stringify(this.ballotForm.getRawValue()));
-        payLoad['SeatID'] = Math.floor(100 + Math.random() * 900)
-        console.log(payLoad)
-        let index = this.seatDict[payLoad.seatName]
-        if (index !== undefined){
-            payLoad.candidates.forEach(candidate =>{
-            this.ballot.seats[index].candidates.push(candidate)
-            console.log(this.ballot.seats[index].candidates)
-        })
-
-            console.log(payLoad.seatName.toString())
-            this.onReset();
-            return
-
-        }
-        this.ballot.seats.push(payLoad)
-
-
-        this.onReset();
-
-            
-            
-
-      
-            
-       
-        // console.log(this.ballot.seats)
-    }
+    // console.log(this.ballot.seats)
+  }
 }
